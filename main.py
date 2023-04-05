162102516a3a5e2d8eb2cfd76a09a95a6d4575db
ankit <ankit.rr528@gmail.com>
2023-04-05 15:04:49 +0530

    added LOF to detect outliers

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 import pandas as pd
 from sklearn.cluster import KMeans
 import numpy as np
+from sklearn.neighbors import LocalOutlierFactor
+import matplotlib.pyplot as plt
 
 df = pd.read_csv("train.csv")
 
-print(df.head)
+# print(np.asarray(df))
+# print(df.columns["category"].unique())
+
 # del df[df.columns[0]]
 X = df.values[:, :-1]
 y = df.values[:, -1]
 
+y = np.asarray(y)
+print(np.unique(y))
+# print(y.)
+
+
+clf = LocalOutlierFactor(n_neighbors=20, contamination=0.1)
+y_pred = clf.fit_predict(X)
+
+# plot the results
+
+plt.scatter(X[:, 0], X[:, 1], c=y_pred, cmap="viridis")
+plt.show()
 # print(df.isnull().sum())
 # kmeans = KMeans(n_clusters=3, random_state=0).fit(X)
 # print(kmeans.labels_.shape)
