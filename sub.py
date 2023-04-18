90a2687c1b26f7dcd2cb5be26ec2450a78a1a82e
ankit <ankit.rr528@gmail.com>
2023-04-18 16:26:09 +0530

    sub.py

--- sub.py
+++ sub.py
@@ -2,8 +2,8 @@ import pandas as pd
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
-from sklearn.neighbors import LocalOutlierFactor
-from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.neighbors import KNeighborsClassifier, LocalOutlierFactor
+from sklearn.model_selection import KFold, cross_val_score
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 import matplotlib.pyplot as plt
 import numpy as np
@@ -21,6 +21,11 @@ y = df["category"]
 X = X.values
 y = y.values
 
+# Applying Knn to have cluster labels as external features
+knn = KNeighborsClassifier(n_neighbors=30)
+knn.fit(X, y)
+cluster_labels = knn.predict(X)
+X = np.hstack((X, cluster_labels.reshape(-1, 1)))
 
 # Applying PCA on the data to reduce dimension of the data
 n_components = 450
@@ -45,33 +50,29 @@ df_test = pd.read_csv("test.csv")
 df_test = df_test.drop(["ID"], axis=1)
 X_test = df_test.values
 
+# using cluster labels as external features
+cluster_labels = knn.predict(X_test)
+X_test = np.hstack((X_test, cluster_labels.reshape(-1, 1)))
+
+
 # applying PCA and LDA on the test data
 X_test = pca.transform(X_test)
 X_test = lda.transform(X_test)
 
 
-# Define a range of hyperparameters to search over
-param_grid = {
-    "C": [0.01, 0.1, 1, 10, 100],
-    "max_iter": [1000, 1500, 3000, 5000, 10000, 10500, 11000],
-}
-
 # Create a logistic regression model
 lr = LogisticRegression()
 
-# Create a grid search object with cross-validation
-grid_search = GridSearchCV(lr, param_grid, cv=8)
 
-# Fit the grid search object to the data
-grid_search.fit(x_train, y_train)
+lr = LogisticRegression(max_iter=10000)
 
-# Get the best hyperparameters
-best_params = grid_search.best_params_
-print("Best hyperparameters:", best_params)
+cv = KFold(n_splits=5, shuffle=True, random_state=42)
 
-# Create a logistic regression model with the
-#  best hyperparameters
-lr = LogisticRegression(**best_params)
+scores = cross_val_score(lr, X, y, cv=cv)
+
+# print the mean and standard deviation of the scores
+print("Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std()))
+
 
 # Fit the model on the training data
 lr.fit(x_train, y_train)
